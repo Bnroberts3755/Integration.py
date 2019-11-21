aa2b41027ee1faf9ca11a43c624e37faed8c063d
Bnroberts3755 <54412579+Bnroberts3755@users.noreply.github.com>
2019-11-21 13:20:07 -0500

    Update Integration.py
    prompt and questions moved down and into main function with start quiz prompt to avoid shadowing of parameter and making things look pretty.

--- Integration.py
+++ Integration.py
@@ -1,74 +1,109 @@
-# Brandon Roberts
-# Space trivia
-# Lines 6-9, 12-23, 25-31, 44-53, "Building a Multiple Choice Quiz - Mike Dane" on YouTube.
-#      -turning the whole quiz into a looping function and pulling from an array made this a whole lot easier.
-#      -altered code where not code-breaking in order to clean up
-# restart function still not complete
-# added check for user input error within questions to check for invalid input
+"""
+This is a quiz program built as a single function.
+__author__ = Brandon Roberts
+Citations added where necessary
+"""
+import sys
 
 
 class Question:
+    """
+
+    """
+
     def __init__(self, prompt, answer):
         self.prompt = prompt
         self.answer = answer
 
 
-questionPrompts = [
-    "What is the branch of science that deals with the physical nature of stars and other celestial bodies?\n"
-    "(a) Space Science\n(b) Cosmology\n(c) Astrophysics\n(d) Physics\n\n",
-    "What type of star is our sun?\n"
-    "(a) Yellow Dwarf\n(b) Red Giant\n(c) Blue Dwarf\n(d) Neutron Star\n\n",
-    "When a star dies, what phenomena occurs?\n"
-    "(a) Solar Eclipse\n(b) Super Novae\n(c) It just stops shining\n(d) Nothing\n\n",
-    "How are black holes formed?\n"
-    "(a) The death of massive stars\n(b) Two stars colliding\n(c) An asteroid flies too fast\n(d) We don't know\n\n",
-    "Which galaxy is the closest to our Milky Way Galaxy?\n"
-    "(a) Whirlpool Galaxy\n(b) Messier 81\n(c) Sombrero Galaxy\n(d) Andromeda Galaxy\n\n"
-]
-
-questions = [
-    Question(questionPrompts[0], "c"),
-    Question(questionPrompts[1], "a"),
-    Question(questionPrompts[2], "b"),
-    Question(questionPrompts[3], "a"),
-    Question(questionPrompts[4], "d"),
-]
-
-
-# added restart = true, restart = false, couldn't test in class
-# def restart():
-#     restart = true
-#     while restart:
-#         restart = input("Would you like to go again? Enter '1' for Yes or '2' for No: ")
-#         if restart == 1:
-#             run_quiz(questions)
-#         else:
-#             print("Thanks for playing!")
-#             restart = false
+# Credit for the basic form of the below function goes to Mike Dane on YouTube,
+# Altered to check for user error by Dalton, Jeffrey, Victoria, Prof. Vaneslow
+# and myself from COP 1500 Fall 2019
 
 
 def run_quiz(questions):
-    print("Welcome to my simple space science quiz!")
-    print("The following is a series of questions meant to stimulate your mind!")
-    print("Make sure you enter the letter at the beginning of each question to ensure credit.")
-    print("Good luck, and let us begin!")
-    total = 0
-    for question in questions:
-        bad_input = True
-        while bad_input:
-            answer = input(question.prompt)
-            if answer == 'a' or answer == 'b' or answer == 'c' or answer == 'd':
-                if answer == question.answer:
-                    total += 1
-                bad_input = False
-            else:
-                bad_input = True
-    print("Congratulations, you got " + str(total) + "/" + str(len(questions)) + " correct!")
-    restart = input("Would you like to play again? Enter '1' for Yes, '2' for No: ")
-    if restart != 1:
-        input("Thank you for playing! Enter any character to exit.")
-    else:
-        run_quiz(questions)
+    """
+Function built to run as a multiple choice quiz without the need for
+multiple lines of code.
+    :param questions:
+    """
+    start_quiz = True
+    while start_quiz:
+        print()
+        print()
+        print("Welcome to my simple space science quiz!")
+        print("The following is a series of questions meant to stimulate your "
+              "mind!")
+        print("Make sure you enter a correctly corresponding letter to ensure "
+              "credit.")
+        print("Good luck, and let us begin!")
+        print()
+        print()
+        total = 0
+        for question in questions:
+            bad_input = True
+            while bad_input:
+                answer = input(question.prompt)
+                if answer == 'a' or answer == 'b' or answer == 'c' or \
+                        answer == 'd':
+                    if answer == question.answer:
+                        total += 1
+                    bad_input = False
+                else:
+                    bad_input = True
+                    print("Please enter a correct response.")
+                    print()
+        print("Congratulations, you got " + str(total) + "/" + str(
+            len(questions))
+              + " correct!")
+        quiz_restart = input("Would you like to play again? Enter 'y' to "
+                             "restart. Enter anything else to exit program.")
+        if quiz_restart != 'y':
+            print("Thank you for playing!")
+            print()
+            sys.exit(0)  # terminates program
+        else:
+            run_quiz(questions)
 
 
-run_quiz(questions)
+def main():
+    question_prompts = [
+        "What is the branch of science that deals with the physical nature of "
+        "stars and other celestial bodies?\n"
+        "(a) Space Science\n(b) Cosmology\n(c) Astrophysics\n(d) Physics\n\n",
+        "What type of star is our sun?\n"
+        "(a) Yellow Dwarf\n(b) Red Giant\n(c) Blue Dwarf\n(d) Neutron Star\n\n",
+        "When a star dies, what phenomena occurs?\n"
+        "(a) Solar Eclipse\n(b) Super Novae\n(c) It just stops shining\n(d) "
+        "Nothing\n\n",
+        "How are black holes formed?\n"
+        "(a) The death of massive stars\n(b) Two stars colliding\n(c) An "
+        "asteroid"
+        " flies too fast\n(d) We don't know\n\n",
+        "Which galaxy is the closest to our Milky Way Galaxy?\n"
+        "(a) Whirlpool Galaxy\n(b) Messier 81\n(c) Sombrero Galaxy\n(d) "
+        "Andromeda"
+        " Galaxy\n\n"
+    ]
+
+    questions = [
+        Question(question_prompts[0], "c"),
+        Question(question_prompts[1], "a"),
+        Question(question_prompts[2], "b"),
+        Question(question_prompts[3], "a"),
+        Question(question_prompts[4], "d"),
+    ]
+    quiz_run = True
+    while quiz_run:
+        quiz_start = input(
+            "Would you like to play a game? Enter 'y' to begin. "
+            "Enter anything else to leave.")
+        if quiz_start != 'y':
+            quiz_run = False
+            print("Thank you for your time!")
+        else:
+            run_quiz(questions)
+
+
+main()
+
